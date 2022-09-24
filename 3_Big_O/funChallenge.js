5a0527b406c6b8dad10d3abd7c7b27ea19fb4266
dindoleonard <dindo.leonard@gmail.com>
2022-09-24 13:17:45 +0800

    Initial Big_O notation

---
+++ 3_Big_O/funChallenge.js
@@ -0,0 +1,48 @@
+/**
+ * CHALLENGE # 1
+ */
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    // O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+// O(n + 1)
+
+// 3 + 4n
+// three steps of O(1)
+// four steps of O(4n)
+// BIG O(3 + 4n)
+
+// or simplified to O(n)
+
+/**
+ * CHALLENGE # 2
+ */
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
+  for (let i = 0; i < input; i++) {
+    // O(n)
+    let x = i + 1; // O(n)
+    let y = i + 2; // O(n)
+    let z = i + 3; // O(n)
+  }
+  for (let j = 0; j < input; j++) {
+    // O(n)
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
+  }
+  let whoAmI = "I don't know"; // O(1)
+}
+
+// O(4 + 7n)
---
+++ 3_Big_O/index.js
@@ -0,0 +1,91 @@
+const nemo = ['nemo'];
+const everyone = [
+  'sadasd',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+  'asdasdas',
+];
+
+const large = new Array(100000).fill('nemo');
+
+/**
+ * O(n)
+ * --> Linear Time
+ * - 1 operation per element in an array
+ */
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === 'nemo') {
+      console.log('found NEMO!');
+    }
+  }
+
+  let t1 = performance.now();
+  console.log('call to find nemo took ' + (t1 - t0) + ' milliseconds');
+}
+findNemo(large);
+
+const boxes = [0, 1, 2, 3, 4, 5];
+const OOne = (array) => {
+  console.log(array[0]);
+};
+OOne(boxes); // 0(1)
+// doing only 1 operation per box's element
+// 0(1) - Contant Time
+
+// BIG O Rule Book (!IMPORTANT)
+/*
+Rule 1: Worst Case
+  Rule 2: Remove Constants
+    Rule 3: Different terms for inputs
+      Rule 4: Drop Non Dominants
+*/
+
+// Rule #3
+function compressBoxesTwice(boxes, boxes2) {
+  boxes.forEach((box) => {
+    console.log(box);
+  });
+
+  boxes2.forEach((box) => {
+    console.log(box);
+  });
+}
+// will result to:
+// O(a + b) or O(n + m)
+// because different terms of inputs, and there are two inputs
+
+/**
+ * NESTED LOOPS (!IMPORTANT)
+ */
+// O(n^2) - Quadratic Time
+// Log all pairs of array
+const boxes3 = ['a', 'b', 'c', 'd', 'e'];
+
+// nested array operation
+const logAllPairsOfArray = (array) => {
+  for (let i = 0; i < array.length; i += 1) {
+    // O(n)
+    for (let j = 0; j < array.length; j += 1) {
+      // O(n)
+      console.log(array[i], array[j]);
+    }
+  }
+};
+logAllPairsOfArray(boxes3);
+// O(n * n) or O(n^2)
+
+// Rule #4
+// if result is O(n + n^2)
+// choose the most dominant term and drop the other, sor result would be:
+// O(n^2)
