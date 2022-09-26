97862695d564800fbe393b8cb22ba1a7b7b4103e
dindoleonard <dindo.leonard@gmail.com>
2022-09-27 03:12:22 +0800

    Interview Questions Problem solving

---
+++ 4_How_to_solve_coding_problems/main.js
@@ -0,0 +1,67 @@
+// Given 2 arrays, create a function that let's a user know (true/false) whether two arrays contain any common items
+// For Example:
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'i'];
+// should return false
+// ----------
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'x'];
+// should return true
+
+/*
+Write key points
+- What are key points?
+ - 2 array
+ - parameters are 2 arrays
+- What is the output?
+ - boolean
+ - true if it 2 array has pair
+ - false if no pair
+- Ask the interviewer the most important value of problem
+ - Do you have time, and space, and memory? etc?
+  - If array is no bigger than 5, you don't have to worry about BigO
+done
+*/
+
+const compareTwoArrayForPairs = (array1, array2) => {
+  for (let i = 0; i < array1.length; i += 1) {
+    for (let j = 0; j < array2.length; j += 1) {
+      if (array1[i] === array2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+// O(a * b);
+// O(n^2) if array size is the same
+const result = compareTwoArrayForPairs(array1, array2);
+console.log(result);
+
+// Tell interviewer this is the brute force
+// Might not be efficient
+// Tell the interviewer why this solution might not be the best
+// Because of quadratic BigO, if size get bigger
+
+// good solution
+/*
+Convert first array to object that looks like
+const obj = {
+  a: true,
+  b: true,
+  c: true,
+  x: true,
+}
+
+then
+just loop through the second array, and check if property/element exist in the object created
+
+array[inde] === obj.properties
+*/
+
+// O(a + b)
+const containsCommonItem2 = (array1, array2) => {
+  // loop throught first array and create object where properties === items in the array
+  // loop through second array and check if item is second array exist on created object
+};
---
+++ 4_How_to_solve_coding_problems/notes.md
@@ -0,0 +1,34 @@
+## What are companies looking for?
+
+1. Analytical Skills
+2. Coding Skills
+3. Technical Skills
+4. Communication Skills
+
+---
+
+## What we need for coding interviews?
+
+- Data Structures
+
+  - Arrays
+  - Stacks
+  - Queues
+  - Linked Lists
+  - Trees
+  - Tries
+  - Graphs
+  - Hash Tables
+
+- Algorithms
+  - Sorting
+  - Dynamic Programming
+  - BFS + DFS (Searching)
+  - Recursion
+
+##
+
+Collection of numbers
+Matching pair that is equal to a sum that is given
+collection [1,2,3,9] sum = 8
+collection [1,2,4,4] sum = 8
