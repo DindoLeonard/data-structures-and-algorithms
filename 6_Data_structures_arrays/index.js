4577584919688f14cf4b3372bd9f5d2d384faf44
dindoleonard <dindo.leonard@gmail.com>
2022-11-28 10:17:47 +0800

    Implementing an Array

--- 6_Data_structures_arrays/index.js
+++ 6_Data_structures_arrays/index.js
@@ -24,3 +24,56 @@ in a[20];
 // create array of 5 and insert
 int b[5] {1,2,3,4,5}
 */
+
+// IMPLEMENTING AN ARRAY
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[index] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1]; //  delete the last item;
+    this.length--; // decrement the length
+  }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('!');
+
+console.log(newArray);
+// newArray.pop();
+// newArray.pop();
+newArray.delete(2);
+newArray.push('are');
+newArray.push('nice');
+
+console.log(newArray);
--- 6_Data_structures_arrays/notes.md
+++ 6_Data_structures_arrays/notes.md
@@ -16,3 +16,13 @@
   - copy and rebuild the array in new location
   - the new array in the new location will be the new array
   - expands as you add more
+
+## classes in javascript
+
+- reference
+  - reference means they are pointing in the same memory
+- instantiation
+  - class extends another class
+    - use super to inherit the fields
+
+## implementing an Array
