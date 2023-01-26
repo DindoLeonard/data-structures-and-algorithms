b2d73016942fc0a74ff7c40c54d60f6cb8594e95
dindoleonard <dindo.leonard@gmail.com>
2023-01-26 14:05:04 +0800

    Linked list: delete reverse methods

--- 8_Data_structures_linked_list/index.js
+++ 8_Data_structures_linked_list/index.js
@@ -1,3 +1,4 @@
+// will change depending on where the reference is pointed to
 // const obj1 = { a: true };
 // const obj2 = obj1;
 // const obj3 = obj2;
@@ -16,15 +17,20 @@ const linkedList = {
       next: {
         value: 16,
         next: null,
+        prev: null,
       },
     },
   },
 };
 
 class Node {
+  value;
+  next;
+  prev;
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
@@ -33,6 +39,7 @@ class LinkedList {
     this.head = {
       value: value,
       next: null,
+      prev: null,
     };
     this.tail = this.head;
     this.length = 1;
@@ -52,20 +59,27 @@ class LinkedList {
 
   append(value) {
     const newNode = new Node(value);
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length += 1;
+    return this;
   }
 
   prepend(value) {
     const newNode = new Node(value);
     newNode.next = this.head;
-
+    this.head.prev = newNode;
     this.head = newNode;
     this.length += 1;
+    return this;
   }
 
   insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
     const newNode = new Node(value);
     /*
     1. traverse
@@ -74,13 +88,15 @@ class LinkedList {
     */
     const PREVIOUS_INDEX = index - 1;
     let leader = this.traverseToIndex(PREVIOUS_INDEX); // getting the data of previous value including its pointer
-    let holdingPointer = leader.next;
+    let follower = leader.next;
 
     leader.next = newNode;
-    newNode.next = holdingPointer;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
     this.length += 1;
 
-    return newNode;
+    return this.printList();
   }
 
   traverseToIndex(index) {
@@ -93,6 +109,79 @@ class LinkedList {
     }
 
     return currentNode;
+  }
+
+  delete(index) {
+    if (index >= this.length) {
+      const LAST_ELEMENT_INDEX = this.length - 1;
+      let beforeTail = this.traverseToIndex(LAST_ELEMENT_INDEX - 1);
+      beforeTail.next = null;
+      return this.printList();
+    }
+
+    const LEADER_INDEX = index - 1;
+    const leader = this.traverseToIndex(LEADER_INDEX);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length = this.length - 1;
+    return this.printList();
+  }
+
+  // [1,2,3,4]
+  // currently only reversing singly linked list
+  reverse() {
+    if (!this.head.next) {
+      return this;
+    }
+
+    this.tail = this.head; // the head become tails
+
+    // storing reference
+    let first = this.head; // first
+    let second = first.next; // second
+
+    // [1,2,3,4,5]
+    while (second) {
+      let temp = second.next; // third
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+
+    this.head.next = null; // since head becomes tail, you need to null the next to terminate or end the linked list
+    this.head = first;
+    return this;
+  }
+
+  reverse2() {
+    this.tail = this.head; // setting tail if there's a tail
+
+    let head = this.head;
+    let prev = null;
+
+    while (head !== null) {
+      let tempNextNode = head.next;
+      head.next = prev;
+      prev = head;
+      head = tempNextNode;
+    }
+
+    this.head = prev;
+
+    return prev;
+  }
+
+  reverse3() {
+    let head = this.head;
+
+    let prev = null;
+
+    while (head !== null) {
+      let nextNode = head.next;
+      head.next = prev;
+      prev = head;
+      head = nextNode;
+    }
   }
 
   // MY WAY
@@ -115,16 +204,52 @@ class LinkedList {
   //     next: this.head.value,
   //   };
   // }
+  // reverse() {
+  //   const arr = [];
+  //   let currentNode = this.head;
+  //   while (currentNode !== null) {
+  //     arr.push(currentNode.value);
+  //     currentNode = currentNode.next;
+  //   }
+  //   let newLinkedList;
+  //   for (let i = this.length - 1; i >= 0; i--) {
+  //     if (i === this.length - 1) {
+  //       newLinkedList = new LinkedList(arr[i]);
+  //     } else {
+  //       newLinkedList.append(arr[i]);
+  //     }
+  //   }
+  //   this.head = newLinkedList.head;
+  //   this.tail = newLinkedList.tail;
+  //   return this.printList();
+  // }
 }
 
-const myLinkedList = new LinkedList(10);
+const myLinkedList = new LinkedList(1);
+myLinkedList.append(2);
+myLinkedList.append(3);
+myLinkedList.append(4);
 myLinkedList.append(5);
-myLinkedList.append(16);
-myLinkedList.append(9);
-myLinkedList.prepend(1);
-myLinkedList.insert(2, 99);
-myLinkedList.insert(4, 88);
-
+// myLinkedList.printList();
+// myLinkedList.insert(1, 99);
+// myLinkedList.prepend(1);
 myLinkedList.printList();
+console.log('reversed');
+myLinkedList.reverse2();
+myLinkedList.printList();
+console.log(myLinkedList);
+// myLinkedList.reverse2();
+// console.log('after-reverse-2');
+// myLinkedList.printList();
+// console.log(myLinkedList);
+// myLinkedList.prepend(1);
+// myLinkedList.insert(2, 99);
+// myLinkedList.insert(20, 100);
+// myLinkedList.insert(4, 88);
+// myLinkedList.printList();
+// myLinkedList.delete(100);
+// myLinkedList.delete(2);
+// myLinkedList.delete(2);
+// myLinkedList.printList();
 
 // console.log(myLinkedList);
--- 8_Data_structures_linked_list/notes.md
+++ 8_Data_structures_linked_list/notes.md
@@ -13,11 +13,14 @@
 - compared to array which is stored in a sequential memory (meaning next to each other), linked list is stored using static memory and doesn't have to be stored next to each other to achieve order
 - linked list is much more loose compared to array
 - bigO of linked list
+
   - prepend O(1)
   - append O(1)
   - lookup O(n)
   - insert O(n)
   - delete O(n)
+
+---
 
 ## psuedo code example
 
@@ -33,6 +36,41 @@ apples
                     372 --> null
 ```
 
+---
+
 ## What is a pointer
 
 - a reference to another place in memory
+
+---
+
+## Types of linked list
+
+- singly
+  - each node only has 1 pionter, the next reference to point out the next node
+  - requires lesser memory
+  - cannot be traversed from the back of the list
+- doubly
+  - each node has 2 pointers, next reference to point out the next node, and previous reference to point out the previous node
+  - requires more memory
+  - can be traversed from front or back
+  - O(n / 2) if you know which half you're gonna start traversing
+
+---
+
+### Reversing singly linked list
+
+- [link_here](https://www.youtube.com/watch?v=NhapasNIKuQ)
+
+---
+
+## PROS & CONS
+
+- Pros
+  1. Fast Insertion
+  2. Fast Deletion
+  3. Ordered
+  4. Flexible Size
+- Cons
+  1. Slow Lookup
+  2. More Memory
